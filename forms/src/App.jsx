fe593edcc09c7ddfbfa29ee85dafb750e3ebb098
jason william <jasonwilliam@jasons-MacBook-Air.local>
2024-03-07 18:03:20 +0530

    done

---
+++ forms/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import React from 'react'
+import Forms from './components/Form'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Forms/>
+    </>
+  )
+}
+
+export default App
